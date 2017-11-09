c39a8e48ac31eb81ca1996b8cf72b267082245e2
quatroka <monteiro.leonardosantos@gmail.com>
2017-11-08 22:41:24 -0200

    update requirements and update functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
+""" File to functional tests. """
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    """ Class test for New Visitor """
 
-assert 'Django' in browser.title
+    def setUp(self):
+        """ Start browser before tests. """
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        """ Close browser after tests. """
+        self.browser.quit()
+
+    def test_can_start_a_list(self):
+        """ Assert if browser shows 'To-Do' in the title """
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
