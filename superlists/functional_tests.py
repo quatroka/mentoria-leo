d79aca696c4b884c1f2242bda9023402ec01f928
quatroka <monteiro.leonardosantos@gmail.com>
2017-11-26 14:52:12 -0200

    refactor all tests, make  a POG and create a model Item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -9,13 +9,17 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         """ Start browser before tests. """
         self.browser = webdriver.PhantomJS()
-        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         """ Close browser after tests. """
         self.browser.quit()
 
-    def test_can_start_a_list(self):
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
         """ Assert if browser can start a list """
         self.browser.get('http://localhost:8000')
 
@@ -24,21 +28,18 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        self.fail('Finish the test!')
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,10 +1,19 @@
+import re
 from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 from django.http import HttpRequest
 from django.test import TestCase
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
+
+    # Workaround to django 1.11 request=request
+    @staticmethod
+    def remove_csfr(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -14,4 +23,36 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        self.remove_csfr(expected_html)
+        self.assertEqual(self.remove_csfr(response.content.decode()), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = self.remove_csfr(render_to_string('home.html',
+                                                          {'new_item_text':  'A new list item'}))
+        self.assertEqual(self.remove_csfr(response.content.decode()), expected_html)
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
