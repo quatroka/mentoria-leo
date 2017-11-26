ec7502971eba0a37aea5aacf9367946336ba067b
quatroka <monteiro.leonardosantos@gmail.com>
2017-11-26 16:33:01 -0200

    fix the tests to pass on travis

--- superlists/lists/functional_tests.py
+++ superlists/lists/functional_tests.py
@@ -1,11 +1,11 @@
 """ File to functional tests. """
-import unittest
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """ Class test for New Visitor """
-
+    
     def setUp(self):
         """ Start browser before tests. """
         self.browser = webdriver.PhantomJS()
@@ -21,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """ Assert if browser can start a list """
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -38,8 +38,3 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
